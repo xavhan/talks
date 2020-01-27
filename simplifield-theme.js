6bc50f93fe8ff4ea130d5f1be571d7c31f90ebb2
Xavier Haniquaut <xavier.haniquaut@simplifield.com>
2020-01-27 16:02:37 +0100

    Add a generic Simplifield Theme using ThemeUI
    https://theme-ui.com/

---
+++ simplifield-theme.js
@@ -0,0 +1,15 @@
+export default {
+  fonts: {
+    body: 'Source Sans Pro, system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  googleFont: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro',
+  colors: {
+    text: '#222222',
+    background: 'ffffff',
+    primary: 'blue',
+    link: '#fd4f57',
+    pre: '#fd4f57',
+    preBackground: '#222222',
+  },
+}
